5d1083d8a2623a8e8dae041baf36bd4560a78633
Ragnar Englund <ragnar.englund@annotell.com>
2023-09-28 15:01:06 +0200

    Comment out unused test

--- hello-service/src/test/scala/MainZIOTest.scala
+++ hello-service/src/test/scala/MainZIOTest.scala
@@ -1,14 +1,15 @@
 import com.kognic.comparison.zio.MainZIO
 import zio.test.{Spec, TestConsole, ZIOSpecDefault, assertTrue}
 
-object MainZIOTest extends ZIOSpecDefault {
-  override def spec = suite("MainZIO")(
-    // We test that the program actually outputs the message
-    test("The message should be correct") {
-      for {
-        _ <- MainZIO.run
-        message <- TestConsole.output
-      } yield assertTrue(message == Vector("Hello World! From ZIO:)\n"))
-    }
-  )
-}
+// Example of testing a ZIO Service coming up
+//object MainZIOTest extends ZIOSpecDefault {
+//  override def spec = suite("MainZIO")(
+//    // We test that the program actually outputs the message
+//    test("The message should be correct") {
+//      for {
+//        _ <- MainZIO.run
+//        message <- TestConsole.output
+//      } yield assertTrue(message == Vector("Hello World! From ZIO:)\n"))
+//    }
+//  )
+//}
