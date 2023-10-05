1b7a0e7bf9a8e5e7aacad902c1e9c93f8612b819
Ragnar Englund <ragnar.englund@annotell.com>
2023-10-05 16:12:50 +0200

    polish FileStorage

--- hello-service/src/main/scala/com/kognic/comparison/vanilla/filestorage/FileStorageImpl.scala
+++ hello-service/src/main/scala/com/kognic/comparison/vanilla/filestorage/FileStorageImpl.scala
@@ -8,29 +8,32 @@ import com.kognic.core.application.SystemActorSystem.Implicits.scheduler
 import spray.json.*
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.io.Source
+import scala.io.{BufferedSource, Source}
 import scala.reflect.io.Path
-import scala.util.{Try, Using}
+import scala.util.Using
 
 class FileStorageImpl(baseDir: Path)(implicit ec: ExecutionContext) extends FileStorage {
-  def getUser(userId: UserId): Future[Either[DomainError, User]] =
-    retry("getUser")(getUserFromFile(userId))
+  def getUser(userId: UserId): Future[Either[DomainError, User]] = {
+    // retry is a custom implementation of retrying a Future, simple use-case but implementation is non-trivial.
+    retry("getUser")(getUserImpl(userId))
       .map(Right(_))
       .recoverWith {
+        // We want to catch this in getUserImpl but cannot, because retry only handles when Future fails
+        // and not when it returns a Left
         case e => Future.successful(Left(NotFoundError(s"User with id $userId not found", e)))
       }
-
-  // Reading from a file does not need a Future, but it would in case we fetch from a database or cloud
-  private def getUserFromFile(userId: UserId): Future[User] = {
-    val path = baseDir / s"user_$userId.json"
-    println(s"Reading user $userId from file")
-    Future.fromTry(readFromFile(path))
   }
 
-  private def readFromFile(path: Path): Try[User] =
-    Using(Source.fromInputStream(getClass.getResourceAsStream(path.toString()))) { source =>
-      source.getLines().mkString.parseJson.convertTo[User]
-    }
+  // Reading from a file does not need a Future, but it would in case we fetch from a database or cloud
+  private def getUserImpl(userId: UserId): Future[User] = {
+    val path = baseDir / s"user_$userId.json"
+    println(s"Reading user $userId from file")
+    Future.fromTry(Using(openSource(path))(parseUser))
+  }
+
+  def openSource(path: Path): BufferedSource = Source.fromInputStream(getClass.getResourceAsStream(path.toString()))
+
+  def parseUser(source: BufferedSource): User = source.getLines().mkString.parseJson.convertTo[User]
 }
 
 object FileStorageImpl {
--- hello-service/src/main/scala/com/kognic/comparison/zio/filestorage/FileStorageZIOImpl.scala
+++ hello-service/src/main/scala/com/kognic/comparison/zio/filestorage/FileStorageZIOImpl.scala
@@ -1,37 +1,47 @@
 package com.kognic.comparison.zio.filestorage
 
 import com.kognic.common.utils.ImplicitConversion.asFiniteDuration
-import com.kognic.comparison.DomainError.NotFoundError
+import com.kognic.comparison.DomainError.{IOError, NotFoundError}
 import com.kognic.comparison.Ids.UserId
 import com.kognic.comparison.{DomainError, User}
 import spray.json.*
-import zio.{Console, Schedule, Scope, URLayer, ZIO, ZLayer, durationInt}
+import zio.{Console, Schedule, URLayer, ZIO, ZLayer, durationInt}
 
 import scala.io.{BufferedSource, Source}
 import scala.reflect.io.Path
 
 case class FileStorageZIOImpl(baseDir: Path) extends FileStorageZIO {
-  def getUser(userId: UserId): ZIO[Any, DomainError, User] =
+  // One line to get exponential backoff
+  private val exponentialBackoffSchedule = (Schedule.exponential(10.milliseconds) >>> Schedule.elapsed).whileOutput(_ < 1.seconds)
+
+  def getUser(userId: UserId): ZIO[Any, DomainError, User] = {
+    getUserImpl(userId).retry(exponentialBackoffSchedule)
+  }
+
+  private def getUserImpl(userId: UserId): ZIO[Any, DomainError, User] =
     ZIO.scoped {
-      val exponentialBackoffSchedule = (Schedule.exponential(10.milliseconds) >>> Schedule.elapsed).whileOutput(_ < 1.seconds)
-      getUserImpl(userId)
-        .retry(exponentialBackoffSchedule)
-        .mapError(e => NotFoundError(s"User with id $userId not found", e))
+      // Some errors we don't want or need to recover from, then we can use orDie which will "let it crash"
+      Console.printLine(s"Reading user $userId from file").orDie *>
+        ZIO.acquireReleaseWith(openSource(userId))(closeSource)(parseUser)
     }
 
-  // Reading from a file does not need a ZIO, but it would in case we fetch from a database or cloud
-  private def getUserImpl(userId: UserId): ZIO[Scope, Throwable, User] = {
+  def parseUser(source: BufferedSource): ZIO[Any, IOError, User] = {
+    // Attempt will catch any exceptions and wrap them in a failed ZIO
+    ZIO.attempt(source.getLines().mkString.parseJson.convertTo[User])
+      .mapError(e => IOError(s"Could not parse file to user", e))
+  }
+
+  private def openSource(userId: UserId): ZIO[Any, NotFoundError, BufferedSource] = {
     val path = baseDir / s"user_$userId.json"
-    Console.printLine(s"Reading user $userId from file") *> readFromFile(path)
+    ZIO.attempt(Source.fromInputStream(getClass.getResourceAsStream(path.toString())))
+      .mapError {
+        case e: NullPointerException => NotFoundError(s"Could not find file with path: $path", e)
+        case e => NotFoundError(s"User with id $userId was not found", e)
+      }
   }
 
-  private def readFromFile(path: Path): ZIO[Scope, Throwable, User] = {
-    def parseUser(source: BufferedSource): ZIO[Any, Throwable, User] = ZIO.attempt(source.getLines().mkString.parseJson.convertTo[User])
-    def openSource: ZIO[Any, Throwable, BufferedSource] = ZIO.attempt(Source.fromInputStream(getClass.getResourceAsStream(path.toString())))
-    def closeSource(source: BufferedSource): ZIO[Any, Nothing, Unit] = ZIO.succeed(source.close())
+  def closeSource(source: BufferedSource): ZIO[Any, Nothing, Unit] = ZIO.succeed(source.close())
 
-    ZIO.acquireReleaseWith(openSource)(closeSource)(parseUser)
-  }
 }
 
 object FileStorageZIOImpl {
