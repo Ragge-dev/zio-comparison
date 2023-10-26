6141a6351a4b19ba2190f5529d29923aaffd5f90
Ragnar Englund <ragnar.englund@annotell.com>
2023-10-26 09:22:51 +0200

    log errors in UserService

--- hello-service/src/main/scala/com/kognic/comparison/vanilla/service/UserServiceImpl.scala
+++ hello-service/src/main/scala/com/kognic/comparison/vanilla/service/UserServiceImpl.scala
@@ -3,15 +3,18 @@ package com.kognic.comparison.vanilla.service
 import com.kognic.comparison.Ids.UserId
 import com.kognic.comparison.vanilla.filestorage.{FileStorage, FileStorageImpl}
 import com.kognic.comparison.{DomainError, User}
+import com.kognic.core.application.DefaultService
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class UserServiceImpl(fileStorage: FileStorage)(implicit ec: ExecutionContext) extends UserService {
+class UserServiceImpl(fileStorage: FileStorage)(implicit ec: ExecutionContext) extends UserService with DefaultService {
   // Expected errors are caught in the Either, unexpected errors causes the Future to fail
   def getUsers(userIds: Seq[UserId]): Future[Either[DomainError, Seq[User]]] =
     for {
-      users <- Future.sequence(userIds.map(fileStorage.getUser))
-    } yield liftError(users)
+      nestedUsers <- Future.sequence(userIds.map(fileStorage.getUser))
+      users = liftError(nestedUsers)
+      _ = logError(users)
+    } yield users
 
   /*
    * We need to lift the errors from the Seq[Either[DomainError, User]] to Either[DomainError, Seq[User]] in order
@@ -21,6 +24,12 @@ class UserServiceImpl(fileStorage: FileStorage)(implicit ec: ExecutionContext) e
     users.foldLeft(Right(Seq()): Either[DomainError, Seq[User]]) {
       case (acc, Right(user)) => acc.map(_ :+ user)
       case (_, Left(e)) => Left(e)
+    }
+
+  private def logError(users: Either[DomainError, Seq[User]]): Unit =
+    users match {
+      case Left(error) => logger.error("Failed to fetch users with error: ", error)
+      case Right(_) => ()
     }
 }
 
