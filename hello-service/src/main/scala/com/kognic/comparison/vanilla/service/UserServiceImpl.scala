9f7f64bf33045e8e65b96e1c0fad0a43b6172d0a
Ragnar Englund <ragnar.englund@annotell.com>
2023-10-27 10:40:49 +0200

    handle UserNotFound in vanilla Scala too

--- hello-service/src/main/scala/com/kognic/comparison/vanilla/service/UserServiceImpl.scala
+++ hello-service/src/main/scala/com/kognic/comparison/vanilla/service/UserServiceImpl.scala
@@ -1,5 +1,6 @@
 package com.kognic.comparison.vanilla.service
 
+import com.kognic.comparison.DomainError.UserNotFoundError
 import com.kognic.comparison.Ids.UserId
 import com.kognic.comparison.{DomainError, User}
 import com.kognic.comparison.vanilla.repo.{UserRepo, UserRepoImpl}
@@ -9,8 +10,17 @@ import scala.concurrent.{ExecutionContext, Future}
 class UserServiceImpl(fileStorage: UserRepo)(implicit ec: ExecutionContext) extends UserService {
   def getUsers(userIds: Seq[UserId]): Future[Either[DomainError, Seq[User]]] =
     for {
-      users <- Future.sequence(userIds.map(fileStorage.getUser))
+      users <- Future.sequence(userIds.map(getUser))
     } yield liftErrors(users)
+
+  private def getUser(userId: UserId): Future[Either[DomainError, User]] =
+    for {
+    user <- fileStorage.getUser(userId)
+  } yield user match {
+      case Left(_: UserNotFoundError) => Right(User(userId, "Default", -1))
+      case Right(user) => Right(user)
+      case Left(error) => Left(error)
+    }
 
   private def liftErrors(users: Seq[Either[DomainError, User]]): Either[DomainError, Seq[User]] =
     users.foldLeft(Right(Seq.empty[User]): Either[DomainError, Seq[User]]) {
